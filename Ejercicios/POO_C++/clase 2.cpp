dec31872ee90792a846d567b30b23411e184268c
barjuegocreador93 <barbosa93cg@gmail.com>
2015-06-17 08:15:54 -0500

    Se sobre carga el operador ==

--- Ejercicios/POO_C++/clase 2.cpp
+++ Ejercicios/POO_C++/clase 2.cpp
@@ -9,6 +9,7 @@ class empleado
 		empleado(string=" ",string=" ",int=18,float=200000);
 		void pedirEmpleado();
 		void mostrarEmpleado();
+		bool operator ==(empleado & otro);
 	private:
 		string codigo;
 		string nombre;
@@ -41,11 +42,25 @@ void empleado::mostrarEmpleado()
 	cout<<"Sueldo: "<<sueldo<<"\n";
 }
 
+bool empleado::operator==(empleado & otro)
+{
+	if(otro.codigo == codigo)
+	{
+		return 1;
+	}
+	return 0;
+}
 
 int main()
 {
-	empleado a;
+	empleado a, b;
 	a.perdirEmpleado();
-	a.mostrarEmpleado();	
+	a.mostrarEmpleado();
+	b.perdirEmpleado();
+	b.mostrarEmpleado();
+	if(a==b)
+	{
+		cout<<"\n\n Usan el mismo codigo!";
+	}	
 	return 0;
 }
